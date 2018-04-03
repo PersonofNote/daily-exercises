9ed1f21c4029ec0720e85e6893a532b77ad1515e
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-04-03 10:35:15 -0700

    Still doesn't handle words of the same length. Have to come back later

--- 4-2-18-longest-word.js
+++ 4-2-18-longest-word.js
@@ -1,16 +1,18 @@
 function LongestWord(sen) { 
     sen = sen.split(" ");
-    var longest = 0;
     for (let i = 0; i < sen.length; i++) {
-      sen[i] = sen[i].replace(/[^\w\s]|_/g, "")
+      sen[i] = sen[i].replace(/[^\w\s]|_/g, "");
       sen[i] = sen[i].replace(/\s+/g, " ");
       console.log(sen[i].length);
+    }
+     var longest = sen[0];
+     for (let i = 0; i < sen.length; i++) {
       if (sen[i].length > longest.length) {
         longest = sen[i];
         //how to handle equal?
       }
       }
-  sen = longest; 
+  sen = longest
   return sen; 
 }
-get = LongestWord("the.....?7777 gregarious, quick brown fox");
+get = LongestWord("four fawn fall");
