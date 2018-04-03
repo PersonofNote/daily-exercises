125c04bb22a8f61c27f132e6ba1c43e385fac66a
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-04-03 10:14:08 -0700

    Much closer, but punctuation is still failing.

--- 4-2-18-longest-word.js
+++ 4-2-18-longest-word.js
@@ -1,15 +1,15 @@
 function LongestWord(sen) { 
     sen = sen.split(" ");
-    let longest = 0;
+    var longest = sen[0];
     for (let i = 0; i < sen.length; i++) {
       console.log(sen[i].length);
-      if (sen[i].length > longest) {
+      if (sen[i].length > longest.length) {
         console.log(sen[i].length + "is longer than" + longest);
-        longest = sen[i].length;
+        longest = sen[i];
       }
       }
-  //console.log(longest + " is the longest word.")
-  // code goes here  
+  console.log(longest + " is the longest word.")
+  sen = longest; 
   return sen; 
 }
 
@@ -21,3 +21,4 @@ function isLetter(str) {
 //LongestWord(readline());                            
 
 get = LongestWord("a world, how are you acclimating?");
+get = LongestWord("punctuation is important..... to skip.")
