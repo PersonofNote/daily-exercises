8ef129208f451883902b8533050da028e0c19e4c
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-04-03 10:27:47 -0700

    Much closer

--- 4-2-18-longest-word.js
+++ 4-2-18-longest-word.js
@@ -1,24 +1,16 @@
 function LongestWord(sen) { 
     sen = sen.split(" ");
-    var longest = sen[0];
+    var longest = 0;
     for (let i = 0; i < sen.length; i++) {
+      sen[i] = sen[i].replace(/[^\w\s]|_/g, "")
+      sen[i] = sen[i].replace(/\s+/g, " ");
       console.log(sen[i].length);
       if (sen[i].length > longest.length) {
-        console.log(sen[i].length + "is longer than" + longest);
         longest = sen[i];
+        //how to handle equal?
       }
       }
-  console.log(longest + " is the longest word.")
   sen = longest; 
   return sen; 
 }
-
-function isLetter(str) {
-  return str.length === 1 && str.match(/[a-z]/i);
-}
-  
-// keep this function call here 
-//LongestWord(readline());                            
-
-get = LongestWord("a world, how are you acclimating?");
-get = LongestWord("punctuation is important..... to skip.")
+get = LongestWord("the.....?7777 gregarious, quick brown fox");
